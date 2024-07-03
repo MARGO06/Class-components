41a021029cf6a5a728ab641c3423847a4924bb5a
Margarita Masko <margaritam996@gmail.com>
2024-07-03 13:06:42 +0300

    feat: install prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,27 +2,39 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:react-hooks/recommended",
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'airbnb',
+    'airbnb-typescript',
+    'plugin:prettier/recommended',
+    'plugin:react/jsx-runtime',
   ],
-  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: {
     ecmaVersion: 6,
-    sourceType: "module",
-    project: ["./tsconfig.json", "./tsconfig.node.json"],
+    sourceType: 'module',
+    project: ['./tsconfig.json', './tsconfig.node.json'],
     ecmaFeatures: {
       jsx: true,
     },
+    tsconfigRootDir: __dirname,
   },
-  parser: "@typescript-eslint/parser",
-  plugins: ["react-refresh", "@typescript-eslint", "react-compiler"],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh', '@typescript-eslint', 'react-compiler', 'prettier'],
   rules: {
-    "react-refresh/only-export-components": [
-      "warn",
-      { allowConstantExport: true },
-    ],
-    "@typescript-eslint/no-explicit-any": "error",
-    "react-compiler/react-compiler": "error",
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    '@typescript-eslint/no-explicit-any': 'error',
+    'react-compiler/react-compiler': 'error',
+    'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
+    'react/jsx-filename-extension': [1, { extensions: ['.jsx', '.tsx'] }],
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        paths: ['src'],
+      },
+    },
   },
 };
